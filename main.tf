94f5ac5e53044105b8afd01c6aa7d3c3e0721321
enchan <enchant0130@gmail.com>
2025-11-15 16:41:24 +0900

    feat: カスタムドメインを設定

--- main.tf
+++ main.tf
@@ -25,18 +25,41 @@ variable "cloudflare_account_id" {
   type = string
 }
 
+variable "worker_name" {
+  type    = string
+  default = "terraform_remote_state_management"
+}
+
+variable "worker_domain" {
+  type    = string
+  default = "terraform_remote_state_management.enchan.me"
+}
+
+variable "cloudflare_zone_id" {
+  type = string
+}
+
 provider "cloudflare" {
   // APIトークンには環境変数 CLOUDFLARE_API_TOKEN の値を使用
 }
 
 resource "cloudflare_worker" "worker" {
   account_id = var.cloudflare_account_id
-  name       = "terraform_remote_state_management"
+  name       = var.worker_name
   observability = {
     enabled = true
   }
   subdomain = {
-    enabled          = true
+    enabled          = false
     previews_enabled = false
   }
+}
+
+resource "cloudflare_workers_custom_domain" "custom_domain" {
+  account_id  = var.cloudflare_account_id
+  environment = "production"
+  hostname    = var.worker_domain
+  service     = var.worker_name
+  zone_id     = var.cloudflare_zone_id
+  depends_on  = [cloudflare_worker.worker]
 }
